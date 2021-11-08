3a8e296a13ff3c7ed0b2c5dd61173251ef97d46e
Luungooo <lungoo.dev@protonmail.com>
2021-11-08 09:50:09 +0100

    Revert "Rewritten to use user inputted numbers"
    This reverts commit 81609cfec8cced4866b3f6482e527e5dae24f7a8.

--- inf_ue4/main.c
+++ inf_ue4/main.c
@@ -2,16 +2,12 @@
 #include "geoAverage.h"
 
 int main() {
-    double n1;
-    double n2;
-    printf("Input Number 1: ");
-    scanf("%lf%*c",&n1);
-    printf("Input Number 2: ");
-    scanf("%lf%*c",&n2);
+    double n1 = 7;
+    double n2 = 6.5;
     double result;
     result = geoAverage(n1, n2);
     printf("Number 1 = %f\n",n1);
     printf("Number 2 = %f\n",n2);
-    printf("Result = %f",result);
+    printf("Result = %f\n",result);
     return 0;
 }
