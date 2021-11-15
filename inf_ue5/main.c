3e4250535492aba4eebf1268dce154978c1b7e15
Luungooo <lungoo.dev@protonmail.com>
2021-11-15 08:51:21 +0100

    Added Uebung 5 Exercise 5 (Expert)

--- inf_ue5/main.c
+++ inf_ue5/main.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int faculty(int n) {
-    int result = 1;
-    if (n < 1) {
+unsigned long long faculty(unsigned long long n) {
+    unsigned long long result = 1;
+    if (n < 0) {
         printf("Error: Numbers can't be negative.\n");
         printf("Exiting Program.\n");
         exit(EXIT_FAILURE);
     }
     if (n > 0) {
-        result = result * n * faculty(n - 1);
+        result = n * faculty(n - 1);
     }
     return result;
 }
 
-int faculty2(int n) {
-    int result = 1;
-    if (n < 1) {
+unsigned long long facultyFor(unsigned long long n) {
+    unsigned long long result = 1;
+    if (n < 0) {
         printf("Error: Numbers can't be negative.\n");
         printf("Exiting Program.\n");
         exit(EXIT_FAILURE);
     }
-    for (int i = n; i > 0; i--) {
+    for (int i = (int) n; i > 0; i--) {
         result = result * i;
     }
     return result;
 }
 
 int main() {
-    int result = faculty2(17);
-    printf("%d", result);
+    unsigned long long result = 1;
+    unsigned long long n = 0;
+    while(ULLONG_MAX / result > n) {
+        n++;
+        result = faculty(n);
+    }
+    printf("Maximum value for n is %llu\n", n);
+    printf("%llu", result);
     return 0;
 }
