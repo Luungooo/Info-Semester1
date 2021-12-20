e5448da0d784f4441065c05451e3b9ea4f3118d4
Luungooo <lungoo.dev@protonmail.com>
2021-12-20 08:21:53 +0100

    Added Uebung 10

---
+++ untitled/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void vertauscheMitPointern(int* pointerA, int* pointerB) {
+    int temp = *pointerA;
+    *pointerA = * pointerB;
+    *pointerB = temp;
+}
+
+int main() {
+    int x = 5;
+    int y = 10;
+    int* pointerX = &x;
+    int* pointerY = &y;
+    vertauscheMitPointern(pointerX,pointerY);
+    printf("x hat nun den Wert %d und y hat den Wert %d.\n", x, y);
+    return 0;
+}
